ecd4ce03ea3c40e79585d1e31350f89b42e40d85
Rohit Parihar <rohitparih@gmail.com>
2023-05-01 14:39:10 +0530

    User Service Api Development Part-4

--- user-service-api/src/main/java/com/bloggios/userService/Controller/AuthController.java
+++ user-service-api/src/main/java/com/bloggios/userService/Controller/AuthController.java
@@ -16,11 +16,42 @@
 
 package com.bloggios.userService.Controller;
 
+import com.bloggios.userService.Payload.ApiResponse;
+import com.bloggios.userService.Payload.AuthRequest;
+import com.bloggios.userService.Service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * @author - rohit
  * @project - Bloggios-Learning-Platform-Backend
  * @package - com.bloggios.userService.Controller
  * @created_on - April 29-2023
  */
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
+    private final AuthService authService;
+
+    //curl -X POST http://localhost:7000/auth/register -H "Content-Type:application/json" -d "{\"email\":\"hello@gmail.com\",\"password\":\"helloworld\"}"
+    @PostMapping(value = "/register")
+    public ResponseEntity<ApiResponse> registerUser(@RequestBody AuthRequest authRequest){
+        logger.info("Entry");
+        ApiResponse apiResponse = authService.registerUser(authRequest);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/register")
+    public ResponseEntity<String> response(){
+        return ResponseEntity.ok("Got Data");
+    }
 }
