60dea561834517fa3a4397b65dc18c4458ab53da
Rohit Parihar <rohitparih@gmail.com>
2023-08-20 15:35:32 +0530

    Cors-Configuration Added

---
+++ api-gateway/src/main/java/com/bloggios/cloud/gateway/CorsConfiguration.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2023 Rohit Parihar and Bloggios
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bloggios.cloud.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * @author - rohit
+ * @project - Bloggios-Learning-Platform-Backend
+ * @package - com.bloggios.cloud.gateway
+ * @created_on - August 20-2023
+ */
+
+@Configuration
+public class CorsConfiguration extends org.springframework.web.cors.CorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:4000"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST"));
+        corsConfig.addAllowedHeader("*");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+}
